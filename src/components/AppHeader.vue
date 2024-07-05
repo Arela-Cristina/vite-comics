c6faca20479d1eaeb609dafd2b2be580d50183a9
Arela-Cristina <instagrandine@gmail.com>
2024-07-05 16:43:02 +0200

    Added menu header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -65,12 +65,19 @@ export default {
 
 <!-- Codice HTML -->
 <template>
+    <!-- logo -->
     <img src="/public/img/dc-logo.png">
+    <!-- menu header -->
+    <div>
+        <ul>
+            <li v-for="(link, index) in links" :key="index"><a href="#">{{ link.text }}</a></li>
+        </ul>
+    </div>
 </template>
 
 <!-- Codice Css -->
 <style scoped>
 img {
-    width: 30%;
+    width: 15%;
 }
 </style>
