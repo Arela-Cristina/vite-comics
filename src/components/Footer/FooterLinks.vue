73f183cee5f9310e61b04e08814b92458e4f906a
Arela-Cristina <instagrandine@gmail.com>
2024-07-08 09:27:42 +0200

    Added style Footer Links

--- src/components/Footer/FooterLinks.vue
+++ src/components/Footer/FooterLinks.vue
@@ -8,12 +8,79 @@ export default {
 
 <!-- Codice HTML -->
 <template>
-    <div>Footer Links</div>
+    <section>
+        <!-- prima elenco di links -->
+        <div class="mainBox">
+            <div class="box">
+                <div>
+                    <span>DC COMICS</span>
+                    <ul>
+                        <li><a href="#">Characters</a></li>
+                    </ul>
+                </div>
+                <!-- scondo elenco di links -->
+                <div>
+                    <span>SHOP</span>
+                    <ul>
+                        <li><a href="#">Characters</a></li>
+                    </ul>
+                </div>
+            </div>
+            <!-- terzo elenco di links -->
+            <div>
+                <span>DC</span>
+                <ul>
+                    <li><a href="#">Characters</a></li>
+                </ul>
+            </div>
+            <!-- cuarto elenco di links -->
+            <div>
+                <span>SITES</span>
+                <ul>
+                    <li><a href="#">Characters</a></li>
+                </ul>
+            </div>
+        </div>
+
+
+        <img src="../../../public/img/dc-logo-bg.png" alt="dc logo">
+    </section>
 </template>
 
 <!-- Codice Css -->
 <style scoped>
-div {
-    font-size: 30px;
+section {
+    position: relative;
+    width: 100%;
+    height: 20rem;
+    background-image: url(../../../public/img/footer-bg.jpg);
+}
+
+.mainBox {
+    display: flex;
+    gap: 3rem;
+    margin-left: 10rem;
+    padding-top: 2rem;
+}
+
+span {
+    font-size: 15px;
+    font-weight: bolder;
+    color: white;
+}
+
+ul,
+a {
+    font-size: 13px;
+    text-decoration: none;
+    list-style: none;
+    color: rgb(151, 151, 151)
+}
+
+img {
+    position: absolute;
+    width: 30rem;
+    top: -5.5rem;
+    left: 47rem;
 }
 </style>
