7d3a07784841a497d1a940260051dd36cb104103
Arela-Cristina <instagrandine@gmail.com>
2024-07-08 10:02:39 +0200

    Added style

--- src/components/Footer/FooterSocial.vue
+++ src/components/Footer/FooterSocial.vue
@@ -8,12 +8,46 @@ export default {
 
 <!-- Codice HTML -->
 <template>
-    <div>Footer Social</div>
+    <section>
+        <span id="signUp">Sign Up Now</span>
+        <div>
+            <span>Follow Us</span>
+            <img src="../../../public/img/footer-pinterest.png" alt="link pinterest">
+        </div>
+    </section>
 </template>
 
 <!-- Codice Css -->
 <style scoped>
+section {
+    background-color: rgb(37, 36, 36);
+    height: 4.5rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-left: 10rem;
+    padding-right: 10rem;
+}
+
+#signUp {
+    border: 2px solid #0282f9;
+    padding: 8px 8px;
+    color: white;
+    font-weight: bolder;
+}
+
 div {
-    font-size: 30px;
+    display: flex;
+    align-items: center;
+    gap: 10px;
+}
+
+span {
+    color:#0282f9;
+    font-weight: bolder;
+}
+
+img {
+    width: 25px;
 }
 </style>
